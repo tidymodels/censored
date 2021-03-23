eeb5b464213ec25a8b77ca3c7a0d4a9d9e33f77f
Hannah Frick <hannah@rstudio.com>
2021-03-23 13:23:10 +0000

    rename and move model spec to parsnip

--- R/cox_reg.R
+++ R/cox_reg.R
@@ -1,6 +1,10 @@
-#' General Interface for Cox Models
+# The model specification is in parsnip.
+
+#' @name proportional_hazards
 #'
-#' `cox_reg()` is a way to generate a _specification_ of a model before
+#' @title Parsnip engines for Proportional Hazards Models
+#'
+#' `proportional_hazards()` is a way to generate a _specification_ of a model before
 #'  fitting and allows the model to be created using different packages in R.
 #'  The main arguments for the model are:
 #' \itemize{
@@ -16,13 +20,9 @@
 #'  functions. If parameters need to be modified, `update()` can be used
 #'  in lieu of recreating the object from scratch.
 #'
-#' @param mode A single character string for the type of model.
-#'  Possible values for this model are "unknown", or "censored regression".
-#' @inheritParams parsnip::linear_reg
-#'
 #' @details
-#'
-#' For `cox_reg()`, the mode will always be "survival".
+#' Proportional hazards models include the Cox model.
+#' For `proportional_hazards()`, the mode will always be "censored regression".
 #'
 #' The model can be created using the `fit()` function using the following _engines_:
 #' \itemize{
@@ -42,53 +42,21 @@
 #'
 #' Note that, for linear predictor prediction types, the results are formatted
 #' for all models such that the prediction _increases_ with time. For the
-#'  proportional hazards model, the sign is reversed.
+#' proportional hazards model, the sign is reversed.
 #'
 #' @examples
-#' parsnip::show_engines("cox_reg")
+#' parsnip::show_engines("proportional_hazards")
 #'
 #' library(survival)
 #'
 #' cox_mod <-
-#'   cox_reg() %>%
+#'   proportional_hazards() %>%
 #'   set_engine("survival") %>%
 #'   fit(Surv(time, status) ~ x, data = aml)
-#' @export
-cox_reg <-
-  function(mode = "censored regression",
-           penalty = NULL,
-           mixture = NULL) {
-
-    args <- list(
-      penalty = enquo(penalty),
-      mixture = enquo(mixture)
-    )
-
-    new_model_spec(
-      "cox_reg",
-      args = args,
-      eng_args = NULL,
-      mode = mode,
-      method = NULL,
-      engine = NULL
-    )
-  }
+NULL
 
 #' @export
-print.cox_reg <- function(x, ...) {
-  cat("Cox Model Specification (", x$mode, ")\n\n", sep = "")
-  parsnip::model_printer(x, ...)
-
-  if (!is.null(x$method$fit$args)) {
-    cat("Model fit template:\n")
-    print(parsnip::show_call(x))
-  }
-
-  invisible(x)
-}
-
-#' @export
-translate.cox_reg <- function(x, engine = x$engine, ...) {
+translate.proportional_hazards <- function(x, engine = x$engine, ...) {
   x <- translate.default(x, engine, ...)
 
   if (engine == "glmnet") {
@@ -102,63 +70,10 @@ translate.cox_reg <- function(x, engine = x$engine, ...) {
   x
 }
 
-# ------------------------------------------------------------------------------
-
-#' @param object A cox model specification.
-#' @param ... Not used for `update()`.
-#' @param fresh A logical for whether the arguments should be
-#'  modified in-place of or replaced wholesale.
-#' @examples
-#' model <- cox_reg(penalty = 10, mixture = 0.1)
-#' model
-#' update(model, penalty = 1)
-#' update(model, penalty = 1, fresh = TRUE)
-#' @method update cox_reg
-#' @rdname cox_reg
-#' @export
-update.cox_reg <-
-  function(object,
-           parameters = NULL,
-           penalty = NULL, mixture = NULL,
-           fresh = FALSE, ...) {
-
-    eng_args <- update_engine_parameters(object$eng_args, ...)
-
-    if (!is.null(parameters)) {
-      parameters <- check_final_param(parameters)
-    }
-    args <- list(
-      penalty = enquo(penalty),
-      mixture = enquo(mixture)
-    )
-
-    args <- update_main_parameters(args, parameters)
-
-    if (fresh) {
-      object$args <- args
-      object$eng_args <- eng_args
-    } else {
-      null_args <- map_lgl(args, null_value)
-      if (any(null_args))
-        args <- args[!null_args]
-      if (length(args) > 0)
-        object$args[names(args)] <- args
-      if (length(eng_args) > 0)
-        object$eng_args[names(eng_args)] <- eng_args
-    }
-
-    new_model_spec(
-      "cox_reg",
-      args = object$args,
-      eng_args = object$eng_args,
-      mode = object$mode,
-      method = NULL,
-      engine = object$engine
-    )
-  }
 
 # ------------------------------------------------------------------------------
 
+# copy of the unexported parsnip:::organize_glmnet_pred()
 organize_glmnet_pred <- function(x, object) {
   if (ncol(x) == 1) {
     res <- x[, 1]
@@ -176,6 +91,7 @@ organize_glmnet_pred <- function(x, object) {
 
 # ------------------------------------------------------------------------------
 
+# copy of the unexported parsnip:::check_penalty()
 # For `predict` methods that use `glmnet`, we have specific methods.
 # Only one value of the penalty should be allowed when called by `predict()`:
 
