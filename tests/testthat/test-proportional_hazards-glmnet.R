4eae66b755fdd3d0f0cfc93dbd31cd83a1c0aa61
Hannah Frick <hannah@rstudio.com>
2021-06-25 10:36:58 +0100

    add tests for `multi_predict()`
    plus a bit of streamlining

--- tests/testthat/test-proportional_hazards-glmnet.R
+++ tests/testthat/test-proportional_hazards-glmnet.R
@@ -40,6 +40,8 @@ test_that("linear_pred predictions", {
   expect_true(all(names(f_pred) == ".pred_linear_pred"))
   expect_equivalent(f_pred$.pred_linear_pred, unname(exp_f_pred))
   expect_equal(nrow(f_pred), nrow(lung2))
+
+  # TODO tests for multi_predict()
 })
 
 # ------------------------------------------------------------------------------
@@ -149,18 +151,53 @@ test_that("survival probabilities - non-stratified model", {
     NA
   )
 
-  pred_2 <- predict(f_fit, new_data = lung2[1:2, ], type = "survival",
+  new_data_3 <- lung2[1:3, ]
+  f_pred <- predict(f_fit, new_data = new_data_3, type = "survival",
                     time = c(100, 200), penalty = 0.1)
 
-  expect_s3_class(pred_2, "tbl_df")
-  expect_equal(names(pred_2), ".pred")
-  expect_equal(nrow(pred_2), 2)
+  expect_s3_class(f_pred, "tbl_df")
+  expect_equal(names(f_pred), ".pred")
+  expect_equal(nrow(f_pred), nrow(new_data_3))
   expect_true(
-    all(purrr::map_lgl(pred_2$.pred, ~ all(dim(.x) == c(2, 2))))
+    all(purrr::map_lgl(f_pred$.pred, ~ all(dim(.x) == c(2, 2))))
   )
   expect_true(
-    all(purrr::map_lgl(pred_2$.pred,
+    all(purrr::map_lgl(f_pred$.pred,
                        ~ all(names(.x) == c(".time", ".pred_survival"))))
+  )
+
+  exp_pred_multi <-
+    predict(f_fit, new_data = new_data_3, type = "survival",
+            time = c(100, 200), penalty = 0.05) %>%
+    tidyr::unnest(cols = .pred) %>%
+    dplyr::mutate(penalty = 0.05,
+                  .row = rep(1:3, each = 2)) %>%
+    dplyr::bind_rows(
+      f_pred %>%
+        tidyr::unnest(cols = .pred) %>%
+        dplyr::mutate(penalty = 0.1, .row = rep(1:3, each = 2))
+    ) %>%
+    dplyr::arrange(.row, .time) %>%
+    dplyr::select(penalty, .time, .pred_survival)
+
+
+  pred_multi <- multi_predict(f_fit, new_data = new_data_3,
+                              type = "survival", time = c(100, 200),
+                              penalty = c(0.05, 0.1))
+  expect_s3_class(pred_multi, "tbl_df")
+  expect_equal(names(pred_multi), ".pred")
+  expect_equal(nrow(pred_multi), nrow(new_data_3))
+  expect_true(
+    all(purrr::map_lgl(pred_multi$.pred,
+                       ~ all(dim(.x) == c(2*2, 3))))
+  )
+  expect_true(
+    all(purrr::map_lgl(pred_multi$.pred,
+                       ~ all(names(.x) == c("penalty", ".time", ".pred_survival"))))
+  )
+  expect_equal(
+    pred_multi %>% tidyr::unnest(cols = .pred),
+    exp_pred_multi
   )
 
 })
@@ -180,18 +217,52 @@ test_that("survival probabilities - stratified model", {
     NA
   )
 
-  pred_2 <- predict(f_fit, new_data = bladder[1:2, ], type = "survival",
+  new_data_3 <- bladder[1:3, ]
+  f_pred <- predict(f_fit, new_data = new_data_3, type = "survival",
                     time = c(5, 10), penalty = 0.1)
 
-  expect_s3_class(pred_2, "tbl_df")
-  expect_equal(names(pred_2), ".pred")
-  expect_equal(nrow(pred_2), 2)
+  expect_s3_class(f_pred, "tbl_df")
+  expect_equal(names(f_pred), ".pred")
+  expect_equal(nrow(f_pred), nrow(new_data_3))
   expect_true(
-    all(purrr::map_lgl(pred_2$.pred, ~ all(dim(.x) == c(2, 2))))
+    all(purrr::map_lgl(f_pred$.pred, ~ all(dim(.x) == c(2, 2))))
   )
   expect_true(
-    all(purrr::map_lgl(pred_2$.pred,
+    all(purrr::map_lgl(f_pred$.pred,
                        ~ all(names(.x) == c(".time", ".pred_survival"))))
+  )
+
+  exp_pred_multi <-
+    predict(f_fit, new_data = new_data_3, type = "survival",
+            time = c(5, 10), penalty = 0.05) %>% ## change the time at the end
+    tidyr::unnest(cols = .pred) %>%
+    dplyr::mutate(penalty = 0.05,
+                  .row = rep(1:3, each = 2)) %>%
+    dplyr::bind_rows(
+      f_pred %>%
+        tidyr::unnest(cols = .pred) %>%
+        dplyr::mutate(penalty = 0.1, .row = rep(1:3, each = 2))
+      ) %>%
+    dplyr::arrange(.row, .time) %>%
+    dplyr::select(penalty, .time, .pred_survival)
+
+  pred_multi <- multi_predict(f_fit, new_data = new_data_3,
+                              type = "survival", time = c(5, 10), ## change the time at the end
+                              penalty = c(0.05, 0.1))
+  expect_s3_class(pred_multi, "tbl_df")
+  expect_equal(names(pred_multi), ".pred")
+  expect_equal(nrow(pred_multi), nrow(new_data_3))
+  expect_true(
+    all(purrr::map_lgl(pred_multi$.pred,
+                       ~ all(dim(.x) == c(2*2, 3))))
+  )
+  expect_true(
+    all(purrr::map_lgl(pred_multi$.pred,
+                       ~ all(names(.x) == c("penalty", ".time", ".pred_survival"))))
+  )
+  expect_equal(
+    pred_multi %>% tidyr::unnest(cols = .pred),
+    exp_pred_multi
   )
 
 })
