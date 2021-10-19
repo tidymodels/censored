fcda68f5a3974854773f2a83cb85f93821da5d61
Hannah Frick <hannah@rstudio.com>
2021-10-19 16:21:11 +0100

    move methods to parsnip
    `survival_reg()`: methods for `translate()` and `check_args()
    `proportional_hazards(): method for `translate()`

--- R/proportional_hazards.R
+++ R/proportional_hazards.R
@@ -83,23 +83,6 @@ fit.proportional_hazards <- function(object,
   res
 }
 
-#' @export
-translate.proportional_hazards <- function(x, engine = x$engine, ...) {
-  x <- translate.default(x, engine, ...)
-
-  if (engine == "glmnet") {
-    # See discussion in https://github.com/tidymodels/parsnip/issues/195
-    x$method$fit$args$lambda <- NULL
-    # Since the `fit` information is gone for the penalty, we need to have an
-    # evaluated value for the parameter.
-    x$args$penalty <- rlang::eval_tidy(x$args$penalty)
-    parsnip::.check_glmnet_penalty_fit(x)
-  }
-
-  x
-}
-
-
 # ------------------------------------------------------------------------------
 
 # notes adapted from parsnip:
