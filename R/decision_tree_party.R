b4f3c1be5066c717d7a50322368a9bbbd1ea77dd
Hannah Frick <hannah@rstudio.com>
2021-10-24 13:45:39 +0100

    engine details for `decision_tree()` + `party`

---
+++ R/decision_tree_party.R
@@ -0,0 +1,9 @@
+#' Decision trees via party
+#'
+#' [party::ctree()] fits a conditional inference decision tree.
+#'
+#' @includeRmd man/rmd/decision_tree_party.Rmd details
+#'
+#' @name details_decision_tree_party
+#' @keywords internal
+NULL
