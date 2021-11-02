8561457da28b7097182408a125857f186c024963
Max Kuhn <mxkuhn@gmail.com>
2021-11-02 10:07:29 -0400

    Apply suggestions from code review
    Co-authored-by: Hannah Frick <hfrick@users.noreply.github.com>

--- R/bag_tree_rpart.R
+++ R/bag_tree_rpart.R
@@ -1,6 +1,6 @@
 #' Ensembles of CART decision trees
 #'
-#' [ipred::bagging()] fits an ensemble of decision trees.
+#' [ipred::bagging()] fits an ensemble of decision trees, using the `rpart` package.
 #'
 #' @includeRmd man/rmd/bag_tree_rpart.Rmd details
 #'
