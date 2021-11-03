162a745414a0870eb2d05470aef63f87e39ca5cf
Hannah Frick <hannah@rstudio.com>
2021-11-03 12:07:49 +0000

    add data template

---
+++ vignettes/articles/template-lung.R
@@ -0,0 +1,13 @@
+#' We'll model the survival of lung cancer patients.
+
+#+ results = "hide", message = FALSE
+library(survival)
+library(tidymodels)
+library(censored)
+tidymodels_prefer()
+
+data(cancer)
+
+lung <- lung %>% drop_na()
+lung_train <- lung[-c(1:5), ]
+lung_test <- lung[1:5, ]
