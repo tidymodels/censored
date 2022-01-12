4129f1fd81f1f326342f47949476431a6970ebd8
Hannah Frick <hannah@rstudio.com>
2022-01-12 15:36:12 +0000

    update `test-survival_reg-survival.R`

--- tests/testthat/test_survival_reg_survival.R
+++ tests/testthat/test_survival_reg_survival.R
@@ -1,122 +1,98 @@
 library(testthat)
-library(parsnip)
-library(survival)
-library(tidyr)
-library(tibble)
-library(dplyr)
-library(flexsurv)
-
-# ------------------------------------------------------------------------------
 
 source(test_path("helper-objects.R"))
 
-basic_form <- Surv(time, status) ~ group
-complete_form <- Surv(time) ~ group
-
-surv_basic <- survival_reg() %>% set_engine("survival")
-surv_lnorm <- survival_reg(dist = "lognormal") %>% set_engine("survival")
-surv_exp <- survival_reg(dist = "exponential") %>% set_engine("survival")
-
-# ------------------------------------------------------------------------------
 
 test_that("survival execution", {
 
-  skip_on_travis()
-
   expect_error(
-    res <- fit(
-      surv_basic,
-      Surv(time, status) ~ age + sex,
-      data = lung,
-      control = ctrl
-    ),
+    res <- survival_reg() %>%
+      set_engine("survival") %>%
+      fit(Surv(time, status) ~ age + sex, data = lung),
     regexp = NA
   )
   expect_output(print(res), "parsnip model object")
+
   expect_error(
-    res <- fit(
-      surv_lnorm,
-      Surv(time) ~ age + sex,
-      data = lung,
-      control = ctrl
-    ),
+    res <- survival_reg(dist = "lognormal") %>%
+      set_engine("survival") %>%
+      fit(Surv(time) ~ age + sex, data = lung),
     regexp = NA
   )
+
   expect_error(
-    res <- fit_xy(
-      surv_basic,
-      x = lung[, c("age", "sex")],
-      y = lung$time,
-      control = ctrl
-    )
+    survival_reg() %>%
+      set_engine("survival") %>%
+      fit_xy(x = lung[, c("age", "sex")], y = lung$time)
   )
 })
 
 test_that("survival time prediction", {
-  skip_on_travis()
+  res <- survival_reg() %>%
+    set_engine("survival") %>%
+    fit(Surv(time, status) ~ age + sex, data = lung)
 
-  res <- fit(
-    surv_basic,
-    Surv(time, status) ~ age + sex,
-    data = lung,
-    control = ctrl
-  )
   exp_pred <- predict(res$fit, head(lung))
-  exp_pred <- tibble(.pred_time = unname(exp_pred))
+  exp_pred <- tibble::tibble(.pred_time = unname(exp_pred))
   expect_equal(exp_pred, predict(res, head(lung)))
+})
+
+test_that("prediction of survival time quantile", {
+  res <- survival_reg() %>%
+    set_engine("survival") %>%
+    fit(Surv(time, status) ~ age + sex, data = lung)
 
   exp_quant <- predict(res$fit, head(lung), p = (2:4) / 5, type = "quantile")
-  exp_quant <-
-    apply(exp_quant, 1, function(x)
-      tibble(.quantile = (2:4) / 5, .pred_quantile = x))
-  exp_quant <- tibble(.pred = exp_quant)
+  exp_quant <- apply(exp_quant, 1, function(x) {
+    tibble::tibble(.quantile = (2:4) / 5, .pred_quantile = x)
+  })
+  exp_quant <- tibble::tibble(.pred = exp_quant)
   obs_quant <- predict(res, head(lung), type = "quantile", quantile = (2:4) / 5)
 
   expect_equal(as.data.frame(exp_quant), as.data.frame(obs_quant))
-
 })
 
-
 test_that("survival probability prediction", {
-  skip_on_travis()
+  res <- survival_reg() %>%
+    set_engine("survival") %>%
+    fit(Surv(time, status) ~ age + sex, data = lung)
 
-  res <- fit(
-    surv_basic,
-    Surv(time, status) ~ age + sex,
-    data = lung,
-    control = ctrl
-  )
   expect_error(
     predict(res, head(lung), type = "survival"),
     "a numeric vector 'time'"
   )
 
   exp_pred <- predict(res, head(lung), type = "survival", time = prob_times)
-  exp_pred_vert <- exp_pred %>% mutate(.patient = row_number()) %>% unnest(cols = .pred)
+  exp_pred_vert <- exp_pred %>%
+    dplyr::mutate(.patient = dplyr::row_number()) %>%
+    tidyr::unnest(cols = .pred)
+
   expect_true(all(names(exp_pred) == ".pred"))
   expect_equal(names(exp_pred_vert), c(".time", ".pred_survival", ".patient"))
 
   # using rms for expected results
-  expect_equal(exp_pred$.pred[[1]]$.pred_survival, rms_surv, tolerance = 0.001)
+  expect_equal(
+    exp_pred$.pred[[1]]$.pred_survival,
+    rms_surv,
+    tolerance = 0.001
+  )
 })
 
-
 test_that("survival hazard prediction", {
-  skip_on_travis()
+  res <- survival_reg() %>%
+    set_engine("survival") %>%
+    fit(Surv(time, status) ~ age + sex, data = lung)
 
-  res <- fit(
-    surv_basic,
-    Surv(time, status) ~ age + sex,
-    data = lung,
-    control = ctrl
-  )
   expect_error(
     predict(res, head(lung), type = "hazard"),
     "a numeric vector 'time'"
   )
 
   exp_pred <- predict(res, head(lung), type = "hazard", time = prob_times)
-  exp_pred_vert <- exp_pred %>% mutate(.patient = row_number()) %>% unnest(cols = .pred)
+  exp_pred_vert <- exp_pred %>%
+    dplyr::mutate(.patient = dplyr::row_number()) %>%
+    tidyr::unnest(cols = .pred)
+
   expect_true(all(names(exp_pred) == ".pred"))
   expect_equal(names(exp_pred_vert), c(".time", ".pred_hazard", ".patient"))
 
