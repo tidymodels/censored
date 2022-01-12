b6cc7324659777349a59a109aac6cb49c66d1584
Hannah Frick <hannah@rstudio.com>
2022-01-12 16:50:50 +0000

    clean up unused objects

--- tests/testthat/helper-objects.R
+++ tests/testthat/helper-objects.R
@@ -1,10 +1,3 @@
-ctrl          <- control_parsnip(verbosity = 1, catch = FALSE)
-caught_ctrl   <- control_parsnip(verbosity = 1, catch = TRUE)
-quiet_ctrl    <- control_parsnip(verbosity = 0, catch = TRUE)
-
-run_glmnet <- utils::compareVersion("3.6.0", as.character(getRversion())) > 0
-
-# ------------------------------------------------------------------------------
 # expected parametric probability results form the rms package for row 1 of
 # the lung data at times 1, 500, and 1000 using weibull model
 
