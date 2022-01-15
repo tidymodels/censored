b9767e8738bf18071a871f391add2fb1dc70176b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-01-14 17:14:35 -0800

    remove engine documentation files

--- R/proportional_hazards_survival.R
+++
@@ -1,9 +0,0 @@
-#' Proportional hazards regression
-#'
-#' [survival::coxph()] fits a Cox proportional hazards model.
-#'
-#' @includeRmd man/rmd/proportional_hazards_survival.Rmd details
-#'
-#' @name details_proportional_hazards_survival
-#' @keywords internal
-NULL
