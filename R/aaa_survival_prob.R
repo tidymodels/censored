53b21ce9c96f57b8a3df5afd97d44d7fc6d21e3d
Hannah Frick <hannah@rstudio.com>
2022-08-11 13:34:26 +0100

    clean up and remove `misc.R`

--- R/aaa_survival_prob.R
+++ R/aaa_survival_prob.R
@@ -128,3 +128,20 @@ pad_survival_na <- function(pred_to_pad,
   res <- tibble(.pred = pred_full)
   res
 }
+
+
+# -------------------------------------------------------------------------
+
+# This function takes a matrix and turns it into list of nested tibbles
+# suitable for predict_survival
+matrix_to_nested_tibbles_survival <- function(x, time) {
+
+  res <- tibble(
+    .row = rep(seq_len(nrow(x)), each = ncol(x)),
+    .time = rep(time, nrow(x)),
+    .pred_survival = as.numeric(t(x))
+  )
+
+  dplyr::group_nest(res, .row, .key = ".pred")$.pred
+}
+
