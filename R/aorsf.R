a99b425af3ad7922021c4e920b2b659db11a1c34
bcjaeger <byron.jaeger@gmail.com>
2022-09-04 12:02:04 -0400

    aorsf update makes the internal calls much simpler - chf not needed yet.

--- R/aorsf.R
+++ R/aorsf.R
@@ -10,55 +10,21 @@
 #' @name aorsf_internal
 #' @examples
 #' library(aorsf)
-#' lung_orsf <- na.omit(lung)
-#' lung_orsf$status <- lung_orsf$status - 1 # aorsf is picky
-#' aorsf <- orsf(Surv(time, status) ~ age + ph.ecog, data = lung_orsf, n_tree = 10)
+#' aorsf <- orsf(na.omit(lung), Surv(time, status) ~ age + ph.ecog, n_tree = 10)
 #' preds <- survival_prob_orsf(aorsf, lung[1:3, ], time = c(250, 100))
-#' preds <- hazard_orsf(aorsf, lung[1:3, ], time = c(250, 100))
 
 survival_prob_orsf <- function(object, new_data, time, ...) {
-  orsf_predict(object, new_data, time, output = 'survival')
-}
-
-#' @rdname aorsf_internal
-#' @export
-hazard_orsf <- function(object, new_data, time, ...) {
-  orsf_predict(object, new_data, time, output = 'hazard')
-}
-
-orsf_predict <- function(object, new_data, time, output){
-
-  output <- rlang::arg_match(output, c("survival", "hazard"))
-
-  switch(
-    output,
-
-    'survival' = {
-     pred_type <- 'surv'
-     .name <- '.pred_survival'
-    },
-
-    'hazard' = {
-      pred_type <- 'chf'
-      .name <- '.pred_hazard'
-    }
-
-  )
-
-  time_ordered <- order(time)
-  pred_horizon <- time[time_ordered] # aorsf wants time in ascending order
 
   res <- predict(object,
                  new_data = new_data,
-                 pred_horizon = pred_horizon,
-                 pred_type = pred_type)
+                 pred_horizon = time,
+                 pred_type = 'surv',
+                 na_action = 'pass',
+                 boundary_checks = FALSE)
 
-  # preds in the same order as time
-  res <- res[, order(time_ordered), drop = FALSE]
+  res <- matrix_to_nested_tibbles_survival(res, time)
 
-  res <- matrix_to_nested_tibbles(res, time, .name = .name)
-
-  # return a tibble?
+  # return a tibble
   tibble(.pred = res)
 
 }
