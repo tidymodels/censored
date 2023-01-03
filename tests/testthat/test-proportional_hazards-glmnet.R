7538a47602169a7572320d5490359c090d685251
Hannah Frick <hannah@rstudio.com>
2023-01-03 17:44:35 +0000

    actually skip on R 3.6

--- tests/testthat/test-proportional_hazards-glmnet.R
+++ tests/testthat/test-proportional_hazards-glmnet.R
@@ -827,7 +827,7 @@ test_that("formula modifications", {
     check_strata_remaining(rlang::expr(x * (y + strata(s)) + z))
   )
 
-  skip_if_not_installed("purrr", minimum_version = "1.0.0")
+  skip_if(R.version$major == "3")
   expect_snapshot(error = TRUE, {
     proportional_hazards(penalty = 0.1) %>%
       set_engine("glmnet") %>%
