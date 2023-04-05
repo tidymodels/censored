3618676aaf879ba796cb199bdb285fe3ab5a2c2e
Hannah Frick <hannah@rstudio.com>
2023-04-05 10:53:03 +0100

    enabled in parsnip
    tidymodels/parsnip#947

--- tests/testthat/test-decision_tree-rpart.R
+++ tests/testthat/test-decision_tree-rpart.R
@@ -86,25 +86,7 @@ test_that("survival predictions", {
 
 # fit via matrix interface ------------------------------------------------
 
-test_that("`fix_xy()` errors", {
-  lung_x <- as.matrix(lung[, c("age", "ph.ecog")])
-  lung_y <- Surv(lung$time, lung$status)
-  lung_pred <- lung[1:5, ]
-
-  spec <- decision_tree() %>%
-    set_engine("rpart") %>%
-    set_mode("censored regression")
-
-  expect_snapshot(error = TRUE, {
-    fit_xy(spec, x = lung_x, y = lung_y)
-  })
-})
-
 test_that("`fix_xy()` works", {
-  skip("until dev version of prodlim is released (current CRAN version: 2019.11.13)")
-  # CRAN prodlim::EventHistory.frame() (called by pec::pecRpart())
-  # can't handle a Surv response which is created outside of the formula
-
   lung_x <- as.matrix(lung[, c("age", "ph.ecog")])
   lung_y <- Surv(lung$time, lung$status)
   lung_pred <- lung[1:5, ]
