2a354c3553ef8b6d668fb659f628348a37baaad7
byron jaeger <byron.jaeger@gmail.com>
2023-12-28 16:50:28 -0500

    parsnip object in survival_prob; see #283

--- R/rand_forest-aorsf.R
+++ R/rand_forest-aorsf.R
@@ -22,7 +22,7 @@ survival_prob_orsf <- function(object, new_data, eval_time, time = deprecated())
   }
 
   pred <- predict(
-    object,
+    object$fit,
     new_data = new_data,
     pred_horizon = eval_time,
     pred_type = "surv",
@@ -31,7 +31,7 @@ survival_prob_orsf <- function(object, new_data, eval_time, time = deprecated())
   )
 
   n_obs <- nrow(new_data)
-  n_eval_time <- length(eval_time) 
+  n_eval_time <- length(eval_time)
 
   res <- data.frame(
     .row = rep(seq_len(n_obs), times = n_eval_time),
