5c310a478082a9264a3165a7b56e86557b7ada43
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-04-09 19:31:18 -0700

    add tests for check_args

--- tests/testthat/test-proportional_hazards.R
+++ tests/testthat/test-proportional_hazards.R
@@ -13,3 +13,10 @@ test_that("arguments", {
   expect_snapshot(translate_args(mixture %>% set_engine("glmnet")))
   expect_snapshot(translate_args(mixture_v %>% set_engine("glmnet")))
 })
+
+test_that("check_args() works", {
+  skip_if_not_installed("parsnip", "1.2.1.9001")
+
+  # Here for completeness, no checking is done
+  expect_true(TRUE)
+})
